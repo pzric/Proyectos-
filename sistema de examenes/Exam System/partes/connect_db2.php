3901e88fbdc67012f722670bd575d4bf78af2276
pzric <35680020+pzric@users.noreply.github.com>
2019-06-03 12:49:14 -0500

    aactualizacion

--- sistema de examenes/Exam System/partes/connect_db2.php
+++
@@ -1,7 +0,0 @@
-<?php
-		$mysqli = new MySQLi("localhost", "root","", "examenes");
-		if ($mysqli -> connect_errno) {
-			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno()
-				. ") " . $mysqli -> mysqli_connect_error());
-		}
-?>
